d5cfd5cb84f21aa8b818a7dcdb9c7719012b6a59
phongichi <88498795+phongichi@users.noreply.github.com>
2022-08-20 15:18:58 +0700

    Add files via upload
    update

---
+++ start.py
@@ -0,0 +1,11 @@
+from input_module import take_input
+from process_module import process
+from output_module import output
+import welcome
+import os
+import assistant_details as ad
+from database import listen_is_on
+from listen_module import listen
+import AssistantGUI as gui
+from automatic import check_last_modify
+
